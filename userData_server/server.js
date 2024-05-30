51ce25026766caf1fde4c55bcdef09567439fcc8
naviadev <qo2oq1020@gmail.com>
2024-05-30 18:52:45 +0900

    user data api sever 테스트

--- userData_server/server.js
+++ userData_server/server.js
@@ -1,10 +1,23 @@
 const http = require('http');
 const fs = require('fs');
-const Database = require('better-sqlite3');
+const DatabaseController = require('./betterSQLite/DB_Controller.js')
 
 
 http.createServer((req,res)=>{
-  
-  console.log(req.url);  
+  let body = ""
+  if(req.url.includes('login') ){
+    req.on("data", data => body += data );
+    req.on("end", () => {
+      let data = JSON.parse(body);
+      loginModule(data);
+    })
+  }
 
-}).listen(3001);
+}).listen(3001, ()=>{console.log('3001 api server start')});
+
+let loginModule = (data)=>{
+  let email = data.email;
+  let pw = data.pw;
+  
+  
+}
