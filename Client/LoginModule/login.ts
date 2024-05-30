19b59f4535fe8538df4f2c8f21340d476f222289
naviadev <qo2oq1020@gmail.com>
2024-05-30 18:52:01 +0900

    login fetch 테스트. local 3001번으로 post 요청

--- Client/LoginModule/login.ts
+++ Client/LoginModule/login.ts
@@ -1,12 +1,15 @@
 export let loginFormEventModule = () => {
   let loginForm = document.getElementById('login-form');
-
+  
   if(!loginForm){
     return
   }else{
     loginForm.addEventListener('submit', async(e) : Promise<void> => {
       e.preventDefault()
-      let i = await fetch(`http://localhost:3001/test`, {method:"GET"})
+      let formTag = e.target as HTMLFormElement;
+      let email = formTag[0] as HTMLInputElement;
+      let pw = formTag[1] as HTMLInputElement;
+      let fetchJson = await fetch(`http://localhost:3001/login`, {method:"POST", body : JSON.stringify({email : email.value, pw : pw.value})})
     })  
   }
 }
