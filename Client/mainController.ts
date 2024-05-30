97f4224d43fd0243677f2c9b1a02a02425b21733
naviadev <qo2oq1020@gmail.com>
2024-05-30 21:53:31 +0900

    main controller content-view customElement 선언

--- Client/mainController.ts
+++ Client/mainController.ts
@@ -1,7 +1,16 @@
-import { loginFormEventModule } from "./LoginModule/login.js"
+import { loginFormEventModule } from "./LoginModule/login.js";
+
+import { ContentView } from "./HTML_Component/content-view.js";
 
 loginFormEventModule()
 
 let mainController = () => {
 
+}
+
+window.onload = ()=>{
+  customElements.define('content-view', ContentView);
+  let content = document.createElement('content-view');
+  let mainView = document.getElementById('main-view') as HTMLDivElement;
+  mainView.insertBefore(content, mainView.children[0]);
 }
