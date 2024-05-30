6dcd3e229c5fcd55f1773f4f9a024929a41ef0f7
naviadev <qo2oq1020@gmail.com>
2024-05-30 21:54:10 +0900

    ts config js

---
+++ Client/HTML_Component/content-view.js
@@ -0,0 +1,21 @@
+export class ContentView extends HTMLElement {
+    constructor() {
+        super();
+        const shadowDom = this.attachShadow({ mode: 'open' });
+        const contentSection = document.createElement('div');
+        contentSection.setAttribute('id', 'content-section');
+        const popularSampleSection = document.createElement('div');
+        popularSampleSection.setAttribute('id', 'popular-sample-section');
+        const newsSection = document.createElement('div');
+        newsSection.setAttribute('id', 'news-section');
+        const leftButton = document.createElement('button');
+        leftButton.innerHTML = 'left';
+        newsSection.appendChild(leftButton);
+        const rightButton = document.createElement('button');
+        rightButton.innerHTML = 'right';
+        newsSection.appendChild(rightButton);
+        shadowDom.appendChild(contentSection);
+        contentSection.appendChild(popularSampleSection);
+        contentSection.appendChild(newsSection);
+    }
+}
