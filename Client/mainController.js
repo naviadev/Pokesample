57b91e91a6dfcc640597f7ef971e206f95890bdf
naviadev <qo2oq1020@gmail.com>
2024-05-30 21:54:01 +0900

    js

--- Client/mainController.js
+++ Client/mainController.js
@@ -1,4 +1,11 @@
 import { loginFormEventModule } from "./LoginModule/login.js";
+import { ContentView } from "./HTML_Component/content-view.js";
 loginFormEventModule();
 let mainController = () => {
+};
+window.onload = () => {
+    customElements.define('content-view', ContentView);
+    let content = document.createElement('content-view');
+    let mainView = document.getElementById('main-view');
+    mainView.insertBefore(content, mainView.children[0]);
 };
