0d97255afcd7dba03e615ab277c2c142b42b6585
naviadev <qo2oq1020@gmail.com>
2024-05-31 15:46:19 +0900

    main controller event module화 (임시)

--- Client/mainController.ts
+++ Client/mainController.ts
@@ -1,39 +1,37 @@
 import { loginFormEventModule } from "./LoginModule/login.js";
 import { ContentView } from "./HTML_Component/content-view.js";
 import { RegisterView } from "./HTML_Component/register-view.js";
-import { registerModule } from './LoginModule/register.js'
+import { registerView } from './LoginModule/register.js'
 
 loginFormEventModule()
 
-let mainController = () => {
-
-}
-
-window.onload = ()=>{
+window.onload = () => {
   customElements.define('content-view', ContentView);
   customElements.define('register-view', RegisterView);
 
-  let content = document.createElement('content-view');
   let mainView = document.getElementById('main-view') as HTMLDivElement;
-
-  mainView.insertBefore(content, mainView.children[0]);
-  // mainView.insertBefore(register, mainView.children[0]);
-
-  let aTag = document.getElementById('register');
-  aTag?.addEventListener('click', ()=>{
-    registerModule(mainView);
-  })
-
+  let content = document.createElement('content-view');
   
-  let logo = document.getElementById('logo');
-  logo?.addEventListener('click', ()=>{
-    let mainView = document.getElementById('main-view') as HTMLDivElement;
-    mainView.removeChild(mainView.children[0])
-    
-    let content = document.createElement('content-view');
-    
-    mainView.insertBefore(content, mainView.children[0]);
-  })
+  mainView.insertBefore(content, mainView.children[0]);
+
+  registerAddEvent(mainView);
+  logoAddEvent(mainView)  
 }
 
 
+let registerAddEvent = (mainView : HTMLDivElement): void =>{
+  let aTag = document.getElementById('register') as HTMLButtonElement;
+  aTag?.addEventListener('click', () => {
+    registerView(mainView);
+  })
+}
+
+let logoAddEvent = (mainView: HTMLDivElement): void =>{
+  let logo = document.getElementById('logo') as HTMLImageElement;
+  logo?.addEventListener('click', () => {
+    let mainView = document.getElementById('main-view') as HTMLDivElement;
+    mainView.removeChild(mainView.children[0]);
+    let content = document.createElement('content-view');
+    mainView.insertBefore(content, mainView.children[0]);
+  })
+}
