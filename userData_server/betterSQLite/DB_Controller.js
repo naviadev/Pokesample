01e8c166d6523985d1664eb8b7da423b0a21d549
naviadev <qo2oq1020@gmail.com>
2024-05-31 15:48:33 +0900

    insert + check 구현. (DB)

--- userData_server/betterSQLite/DB_Controller.js
+++ userData_server/betterSQLite/DB_Controller.js
@@ -1,13 +1,30 @@
 const db = require('better-sqlite3')
 
-class DatabaseController{
-  constructor(){
+class DatabaseController {
+  constructor() {
     this.db = new db('./db/user_data.db', { verbose: console.log });
   }
-  InsertUserData(mail, pw){
-    let register = this.db.prepare('insert into user (email, password) values (?, ?)')
-    register.run(mail,pw)
-  }
-}
 
+  insertUserData(mail, name, pw) {
+    let register = this.db.prepare('insert into user (email, name, password) values (?, ?, ?)');
+    register.run(mail, name, pw);
+    this.db.close();
+  }
+
+  checkUserEmail(email) {
+    let checkEmail = this.db.prepare(`SELECT * FROM user where email = ?`)
+    let result = checkEmail.get(email);
+    
+    result !== undefined ? 
+    result = true : 
+    result = false;
+
+    this.db.close();
+
+    return result;
+  }
+
+
+
+}
 module.exports = DatabaseController;
