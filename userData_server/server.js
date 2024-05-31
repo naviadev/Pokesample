580985fb5f80643fe38d5678fd8ae59cc0f01fa1
naviadev <qo2oq1020@gmail.com>
2024-05-31 16:44:13 +0900

    server 모듈 수정 (express 주석 삭제, db 모듈로 부터 반환받는 boolean 삭제로 인해 코드 수정 )

--- userData_server/server.js
+++ userData_server/server.js
@@ -3,17 +3,11 @@ const fs = require('fs');
 const DatabaseController = require('./betterSQLite/DB_Controller.js');
 const { register } = require('module');
 
-// const { express } = require('express');
-// const { cors } = require('cors')
-
-// const app = express();
-
-// app.use(cors({
-//   origin : '*'
-// }))
 
 http.createServer((req, res) => {
   let body = ""
+
+  console.log(req.url);
 
   if (req.url.includes('login')) {
     req.on("data", data => body += data);
@@ -23,24 +17,24 @@ http.createServer((req, res) => {
     })
   }
 
+
   if (req.url.includes('register')) {
     req.on("data", data => body += data);
     req.on("end", () => {
       let data = JSON.parse(body);
-      registerModule(data);
+      registerModule(data, res);
     })
   }
 
   // GET
   if (req.url.includes('emailCheck')) {
     let email = req.url.split('/emailCheck/')
-
     let db = new DatabaseController();
     let resultBool = db.checkUserEmail(email[1]);
-    
+
     resultBool ?
-      res.writeHead(200, {'Access-Control-Allow-Origin' : '*'}) :
-      res.writeHead(404, {'Access-Control-Allow-Origin' : '*'}) ;
+      res.writeHead(200, { 'Access-Control-Allow-Origin': '*' }) :
+      res.writeHead(201, { 'Access-Control-Allow-Origin': '*' });
 
     res.end();
   }
@@ -52,9 +46,16 @@ let loginModule = (data) => {
   let pw = data.pw;
 }
 
-let registerModule = (data) => {
+let registerModule = (data, res) => {
   let name = data.name;
   let email = data.email;
   let emailURL = data.emailURL;
   let pw = data.pw;
+
+  let db = new DatabaseController();
+  db.insertUserData(`${email}@${emailURL}`, name, pw)
+
+  res.writeHead(200, { 'Access-Control-Allow-Origin': '*' })
+
+  res.end()
 }
