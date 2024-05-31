e67a0348a80055119aaec3a8d5f02a2b481ec772
naviadev <qo2oq1020@gmail.com>
2024-05-31 11:31:21 +0900

    register view test

--- Client/mainController.js
+++ Client/mainController.js
@@ -1,11 +1,15 @@
 import { loginFormEventModule } from "./LoginModule/login.js";
 import { ContentView } from "./HTML_Component/content-view.js";
+import { RegisterView } from "./HTML_Component/register-view.js";
 loginFormEventModule();
 let mainController = () => {
 };
 window.onload = () => {
     customElements.define('content-view', ContentView);
-    let content = document.createElement('content-view');
+    customElements.define('register-view', RegisterView);
+    let register = document.createElement('register-view');
+    // let content = document.createElement('content-view');
     let mainView = document.getElementById('main-view');
-    mainView.insertBefore(content, mainView.children[0]);
+    // mainView.insertBefore(content, mainView.children[0]);
+    mainView.insertBefore(register, mainView.children[0]);
 };
