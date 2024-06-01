08f7067bf72d1662c4a91c15aadbd8e20b50f0b8
naviadev <qo2oq1020@gmail.com>
2024-06-01 13:37:42 +0900

    쿠키 전송 추가

--- userData_server/server.js
+++ userData_server/server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const fs = require('fs');
 const DatabaseController = require('./betterSQLite/DB_Controller.js');
-const { register } = require('module');
+const crypto = require('crypto');
+const Database = require('better-sqlite3');
 
 
 http.createServer((req, res) => {
@@ -11,12 +12,14 @@ http.createServer((req, res) => {
 
   if (req.url.includes('login')) {
     req.on("data", data => body += data);
+
     req.on("end", () => {
+      
       let data = JSON.parse(body);
-      loginModule(data);
+      
+      loginModule(data, res);
     })
   }
-
 
   if (req.url.includes('register')) {
     req.on("data", data => body += data);
@@ -41,11 +44,28 @@ http.createServer((req, res) => {
 
 }).listen(3001, () => { console.log('3001 api server start') });
 
-let loginModule = (data) => {
+
+
+
+
+let loginModule = (data, res) => {
+  let byteLength = 12;
   let email = data.email;
   let pw = data.pw;
-}
 
+  let db = new DatabaseController();
+  let userObj = db.checkLoginData(email, pw);
+  console.log(userObj)
+  if(userObj !== undefined && userObj !== null){
+    let randKey = crypto.randomBytes(byteLength).toString('hex');
+    res.writeHead(201, { 'Set-Cookie' : `${randKey}` , 'Access-Control-Allow-Origin': '*'});
+    res.end();
+  }else{
+    res.writeHead(204, { 'Set-Cookie' : `${randKey}` , 'Access-Control-Allow-Origin': '*'});
+    res.end();
+  }
+}
+  
 let registerModule = (data, res) => {
   let name = data.name;
   let email = data.email;
