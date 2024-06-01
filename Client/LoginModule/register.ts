7a35026a44e460dd6c318da15e5179408bd92f54
naviadev <qo2oq1020@gmail.com>
2024-06-01 13:39:13 +0900

    register 버튼 이벤트 모듈화

--- Client/LoginModule/register.ts
+++ Client/LoginModule/register.ts
@@ -1,4 +1,11 @@
-export let registerView = (parent : HTMLDivElement) => {
+export let registerAddEvent = (mainView : HTMLDivElement): void =>{
+  let aTag = document.getElementById('register') as HTMLButtonElement;
+  aTag?.addEventListener('click', () => {
+    registerView(mainView);
+  })
+}
+
+let registerView = (parent : HTMLDivElement) => {
   parent.removeChild(parent.children[0])
   let register = document.createElement('register-view');
   parent.insertBefore(register, parent.children[0]);
