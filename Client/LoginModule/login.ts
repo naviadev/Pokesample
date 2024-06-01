b184d79980beb251a11c84211c959215b5681989
naviadev <qo2oq1020@gmail.com>
2024-06-01 13:39:02 +0900

    쿠키 요청 부분 추가

--- Client/LoginModule/login.ts
+++ Client/LoginModule/login.ts
@@ -9,7 +9,18 @@ export let loginFormEventModule = () => {
       let formTag = e.target as HTMLFormElement;
       let email = formTag[0] as HTMLInputElement;
       let pw = formTag[1] as HTMLInputElement;
-      let fetchJson = await fetch(`http://localhost:3001/login`, {method:"POST", body : JSON.stringify({email : email.value, pw : pw.value})})
+
+      let loginRes = fetch(`http://localhost:3001/login`, {method:"POST", body : JSON.stringify({email : email.value, pw : pw.value})}).then(
+        (result)=>{
+          // console.log(result)
+          // 쓰기 작업, 읽기 작업. 절차가 비어있다. 
+          // 쿠키, key value 셋업부터. 
+          // front -> 접근자 writealbe? 켜져있는지.
+          // document.cookie = 
+        }
+      )
+
+      
     })  
   }
 }
