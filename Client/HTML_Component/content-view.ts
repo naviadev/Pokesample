e3639b097551b7a76ccf7cb3b6eb989ec77208a8
naviadev <qo2oq1020@gmail.com>
2024-06-02 14:55:21 +0900

    Content View 오타 수정

--- Client/HTML_Component/content-view.ts
+++ Client/HTML_Component/content-view.ts
@@ -1,14 +1,12 @@
 export class ContentView extends HTMLElement {
   constructor() {
     super();
-
     const shadowDom = this.attachShadow({ mode: 'open' });
-
     const contentSection = document.createElement('div');
-    contentSection.setAttribute('id', 'content-section');
 
+    contentSection.setAttribute('id', 'content-section');
     const popularSampleSection = document.createElement('div');
-    popularSampleSection.setAttribute('id', 'popular-sample-section'); 
+    popularSampleSection.setAttribute('id', 'popular-sample-section');
 
     const newsSection = document.createElement('div');
     newsSection.setAttribute('id', 'news-section');
@@ -25,42 +23,45 @@ export class ContentView extends HTMLElement {
     shadowDom.appendChild(contentSection);
     contentSection.appendChild(popularSampleSection);
     contentSection.appendChild(newsSection);
-    
   }
 
-  setStyleContentView(){
+  setStyleContentView() {
     let style = document.createElement('style');
     style.textContent = `
-    #content-section{
-      display: grid;
-      grid-template-rows: 2fr 3fr;
-      grid-template-areas:
-        "popular"
-        "news";
-      gap: 30px;
-    
-      // popular-sample-section
-      >:nth-child(1) {
-        grid-area: popular;
-        border: solid 1px;
-        border-radius: 40px;
-        @include flexAlign;
+      #content-section {
+        display: grid;
+        grid-template-rows: 2fr 3fr;
+        grid-template-areas:
+          "popular"
+          "news";
         gap: 30px;
-    
-        >div {
-          border-radius: 40px;
-          width: 200px;
-          height: 80%;
-          background-color: rgb(180, 180, 180);
-        }
-    }
-    
-    }
-    `
+      }
+      
+      #popular-sample-section {
+        grid-area: popular;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+      }
+      
+      #popular-sample-section > div {
+        width: 200px;
+        height: 80%;
+        background-color: rgb(180, 180, 180);
+      }
+
+      #news-section {
+        grid-area: news;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+
+      #news-section button {
+        width: 100px;
+        height: 40px;
+      }
+    `;
     return style;
   }
 }
-
-
-
-
