c70048dad811299714ed5d8e9945a1cc71bfceb3
naviadev <qo2oq1020@gmail.com>
2024-06-02 14:55:05 +0900

    login fetch -> credentials : include 를 추가하여 Cookie 요청을 성공적으로 진행할 수 있도록 추가하였다.

--- Client/LoginModule/login.ts
+++ Client/LoginModule/login.ts
@@ -10,17 +10,14 @@ export let loginFormEventModule = () => {
       let email = formTag[0] as HTMLInputElement;
       let pw = formTag[1] as HTMLInputElement;
 
-      let loginRes = fetch(`http://localhost:3001/login`, {method:"POST", body : JSON.stringify({email : email.value, pw : pw.value})}).then(
-        (result)=>{
-          // console.log(result)
-          // 쓰기 작업, 읽기 작업. 절차가 비어있다. 
-          // 쿠키, key value 셋업부터. 
-          // front -> 접근자 writealbe? 켜져있는지.
-          // document.cookie = 
-        }
-      )
-
-      
+      let loginRes = await fetch(`http://localhost:3001/login`, {
+        method:"POST",
+        body : JSON.stringify({email : email.value, pw : pw.value}),
+        credentials : 'include',
+        }).then(
+          (result)=>{ alert(`성공 ${result}`)}
+          // View 전환 필요.
+        )      
     })  
   }
 }
