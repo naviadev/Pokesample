032b0cd6e37fdc4afed634b9cfe9647fe1aba5c1
naviadev <qo2oq1020@gmail.com>
2024-06-04 16:32:10 +0900

    로그인 완료 view 출력을 위한 class script 생성

---
+++ Client/HTML_Component/user-view.ts
@@ -0,0 +1,10 @@
+export class UserView extends HTMLElement {
+  constructor() {
+    super();
+    const shadowDom = this.attachShadow({ mode: 'open' });
+    const UserDiv = document.createElement('div');
+    
+  
+  }
+
+}
