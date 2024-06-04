0d50ec8fc6db70417077105132126381961caf39
naviadev <qo2oq1020@gmail.com>
2024-06-04 15:55:52 +0900

    로그인 실패 알림 추가

--- Client/LoginModule/login.ts
+++ Client/LoginModule/login.ts
@@ -1,23 +1,28 @@
 export let loginFormEventModule = () => {
-  let loginForm = document.getElementById('login-form');
   
-  if(!loginForm){
-    return
-  }else{
-    loginForm.addEventListener('submit', async(e) : Promise<void> => {
-      e.preventDefault()
+  let loginForm = document.getElementById("login-form");
+
+  if (!loginForm) {
+    return;
+  } else {
+    loginForm.addEventListener("submit", async (e): Promise<void> => {
+      e.preventDefault();
       let formTag = e.target as HTMLFormElement;
       let email = formTag[0] as HTMLInputElement;
       let pw = formTag[1] as HTMLInputElement;
 
       let loginRes = await fetch(`http://localhost:3001/login`, {
-        method:"POST",
-        body : JSON.stringify({email : email.value, pw : pw.value}),
-        credentials : 'include',
-        }).then(
-          (result)=>{ alert(`성공 ${result}`)}
-          // View 전환 필요.
-        )      
-    })  
+        method: "POST",
+        body: JSON.stringify({ email: email.value, pw: pw.value }),
+        credentials: "include",
+      }).then(
+        (result) => {
+          alert(`성공 ${result}`);
+        },
+        (error) => {
+          alert(`${error} 실패`);
+        }
+      );
+    });
   }
-}
+};
