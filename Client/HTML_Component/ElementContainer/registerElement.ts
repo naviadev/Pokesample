0657f5ca01ced05056ddde86483df62e153536ce
naviadev <qo2oq1020@gmail.com>
2024-06-04 14:39:55 +0900

    customElement.ts에 생성자 매개변수로 들어갈 파일이 명시되어있다.

---
+++ Client/HTML_Component/ElementContainer/registerElement.ts
@@ -0,0 +1,14 @@
+export const DomElementContainer = {
+  "register-section": {
+    'header-h2':[],
+    'name-input':[],
+    'email-div':{
+      'email-input' : [],
+      'at':[],
+      'url-input' : []
+    },
+    'password-input':[],
+    'pwcheck-input':[],
+    'submit-button':[],
+  },
+};
