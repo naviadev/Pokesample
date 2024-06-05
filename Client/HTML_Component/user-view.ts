39f31f5181c0b0fd98a064c0c04b3580a8cca4b1
naviadev <qo2oq1020@gmail.com>
2024-06-06 00:03:16 +0900

    login 이후 해당 섹션에 추가될 html 컴포넌트 파일 생성

--- Client/HTML_Component/user-view.ts
+++ Client/HTML_Component/user-view.ts
@@ -1,10 +1,26 @@
 export class UserView extends HTMLElement {
   constructor() {
     super();
+    // this.createDom();
+  }
+
+  createDom(): void {
+    let loginSection = document.getElementById('login-section');
+
     const shadowDom = this.attachShadow({ mode: 'open' });
+
     const UserDiv = document.createElement('div');
-    
-  
+    const nameTag = document.createElement('a');
+    const logoutButton = document.createElement('button');
+
+    let child = loginSection?.children;
+
+    if (child) {
+      for (let i = 0; i < child?.length; i++) {
+        document.removeChild(child[i]);
+      }
+    }
+
   }
 
 }
